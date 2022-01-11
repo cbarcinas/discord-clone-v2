f067b8b25f6e730fb0148033bde56eaa43dab2c8
Craig Barcinas <cbarcinas@live.com>
2022-01-10 21:51:52 -0800

    added comments to store.js

--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/userSlice";
 import appReducer from "../features/appSlice";
 
+// The redux store is the data layer surrounding our application,
+// all state (inside any reducers) can be accessed through our store.
+// If we need to grab the user, after they login, to be used in another
+// component, we can do that by accessing the store.
+
 export default configureStore({
   reducer: {
     user: userReducer,
