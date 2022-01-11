f333239eee991e0d8f2cfa8d529e9d080f23b9ca
Craig Barcinas <cbarcinas@live.com>
2022-01-10 22:29:38 -0800

    created ternary to display app,  current wip

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import "./App.css";
+import { useSelector } from "react-redux";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
+import { selectUser } from "./features/userSlice";
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
-      <Sidebar />
-
-      <Chat />
+      {/* we use a ternary condition to display the sidebar
+      and chat components if the user exists. We get the 
+      user once the login through the login component */}
+      {user ? (
+        <>
+          <Sidebar />
+          <Chat />
+        </>
+      ) : null}
     </div>
   );
 }
