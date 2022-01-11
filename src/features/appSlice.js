7c50f5baf83f3af5765356cef07cdb5d1f5a1d95
Craig Barcinas <cbarcinas@live.com>
2022-01-10 22:03:22 -0800

    fixed appSlice

--- src/features/appSlice.js
+++ src/features/appSlice.js
@@ -1,28 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const appSlice = createSlice({
-  // a name, used in action types
   name: "app",
-  // the initial state of the reducer
   initialState: {
     channelId: null,
-    channelName: null,
+    channelname: null,
   },
-  // an object of 'case reducers'
   reducers: {
-    // these functions are intended to handle a specific action type
-    // these functions (login / logout) are equivalent to a case statement in a switch
     setChannelId: (state, action) => {
       state.app += action.payload;
     },
   },
 });
 
-// we are exporting login & logout so that they can
-// be used anywhere in our application
 export const { setChannelId } = appSlice.actions;
 
-// exporting our reducer so they can be used in our application
 export const selectChannelId = (state) => state.app.channelId;
 export const selectChannelName = (state) => state.app.channelName;
 
